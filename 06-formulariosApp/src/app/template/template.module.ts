ef5b5b18b9d2768d1ad3a3884eafb2c3f0946319
jalarrosap <jalarrosap@eservicios.indra.es>
2022-01-16 10:59:13 +0100

    Subimos...

--- 06-formulariosApp/src/app/template/template.module.ts
+++ 06-formulariosApp/src/app/template/template.module.ts
@@ -6,14 +6,17 @@ import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { BasicosComponent } from './basicos/basicos.component';
 import { SwitchesComponent } from './switches/switches.component';
 import { FormsModule } from '@angular/forms';
+import { CustomMinDirective } from './directives/custom-min.directive';
 
 
 @NgModule({
   declarations: [
     DinamicosComponent,
     BasicosComponent,
-    SwitchesComponent
+    SwitchesComponent,
+    CustomMinDirective
   ],
+  
   imports: [
     CommonModule,
     FormsModule, //esto hace que el submit no haga el refresh de la página
