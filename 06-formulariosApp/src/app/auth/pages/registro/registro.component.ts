46a35c2f14254d43bc2d11960b8a74fb5f97b8c2
JuanAlejandroLarrosaPinar <alejandro.larrosa.pinar@gmail.com>
2022-02-13 10:48:25 +0100

    Subimos...

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 //import { nombreApellidoPattern, emailPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
@@ -15,17 +16,55 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['',[Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
-    email: ['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
-    username: ['dd',[ Validators.required, this.validatorService.noPuedeSerStrider], ]
+    email: ['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)],[this.emailValidatorService]],
+    username: ['',[ Validators.required, this.validatorService.noPuedeSerStrider, ],],
+    password: ['', [Validators.required, Validators.minLength(6)],],
+    password2: ['ee', [Validators.required]],
+
+  },{
+    validators:[
+      this.validatorService.camposIguales('password', 'password2')
+    ]
   });
 
-  constructor(private fb: FormBuilder, private validatorService: ValidatorService) { }
+
+  get emailErrorMsg(): string{
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors!=null && errors['required']){
+      return 'El email es obligatorio';
+    }
+    if(errors!=null && errors['pattern']){
+      return 'El patrón no es correcto';
+    }
+    if(errors!=null && errors['emailTomado']){
+      return 'El email ya fue asignado';
+    }
+    
+    return '';
+  }
+
+  get usernameErrorMsg(): string{
+    const errors = this.miFormulario.get('username')?.errors;
+    if(errors!=null && errors['required']){
+      return 'El username es obligatorio';
+    }
+    if(errors!=null && !errors['noStrider']){
+      return 'El username no puede ser strider';
+    }
+    
+    return '';
+  }
+
+  constructor(private fb: FormBuilder, private validatorService: ValidatorService,
+    private emailValidatorService: EmailValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
-      nombre:'alejandro larrosa',
-      email:'pepe',
-      username:'hola'
+      //nombre:'alejandro larrosa',
+      //email:'pepe',
+      //username:'hola',
+      //password:'a',
+      //password2:'a',
     });
   }
 
@@ -33,6 +72,31 @@ export class RegistroComponent implements OnInit {
     return this.miFormulario.get(campo)?.invalid
     && this.miFormulario.get(campo)?.touched;
   }
+
+  /*
+  emailRequired(){
+    const touched = this.miFormulario.get('email')?.touched;
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors!=null){
+      return touched && errors['required'];
+    }
+  }
+
+  emailFormato(){
+    const touched = this.miFormulario.get('email')?.touched;
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors!=null){
+      return touched && errors['pattern'];
+    }
+  }
+
+  emailTomado(){
+    const touched = this.miFormulario.get('email')?.touched;
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors!=null){
+      return touched && errors['emailTomado'];
+    }
+  }*/
 
   submitFormulario(){
     console.log(this.miFormulario.value);
--- 06-formulariosApp/src/app/shared/validator/validator.service.ts
+++ 06-formulariosApp/src/app/shared/validator/validator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormControl, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +11,29 @@ export class ValidatorService {
 
   constructor() { }
 
-   noPuedeSerStrider (control: FormControl) : ValidationErrors | null {
+  noPuedeSerStrider(control: FormControl): ValidationErrors | null {
     const valor = control.value?.trim().toLowerCase();
     console.log(valor);
     if (valor === 'strider') {
-        return {
-            noStrider: false
-        }
+      return {
+        noStrider: false
+      }
     }
     return null;
-}
+  }
+
+  camposIguales(campo1: string, campo2: string){
+    return (formGroup: AbstractControl): ValidationErrors | null =>{
+      const pass1 = formGroup.get(campo1)?.value;
+      const pass2 = formGroup.get(campo2)?.value;
+      if(pass1!==pass2){
+        formGroup.get(campo2)?.setErrors({noIguales: true})
+        return {
+          noIguales: true
+        }
+      }
+      formGroup.get(campo2)?.setErrors(null)
+      return null;
+    }
+  }
 }
